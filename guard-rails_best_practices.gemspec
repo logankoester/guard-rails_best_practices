3c9e28e084619f3814a53098991be0e2e2207a42
Logan Koester <lkoester@agoragames.com>
2011-10-06 12:45:20 -0400

    Works

--- guard-rails_best_practices.gemspec
+++ guard-rails_best_practices.gemspec
@@ -1,6 +1,6 @@
 # -*- encoding: utf-8 -*-
 $:.push File.expand_path("../lib", __FILE__)
-require "guard-rails_best_practices/version"
+require "guard/guard-rails_best_practices/version"
 
 Gem::Specification.new do |s|
   s.name        = "guard-rails_best_practices"
@@ -8,8 +8,8 @@ Gem::Specification.new do |s|
   s.authors     = ["Logan Koester"]
   s.email       = ["lkoester@agoragames.com"]
   s.homepage    = ""
-  s.summary     = %q{TODO: Write a gem summary}
-  s.description = %q{TODO: Write a gem description}
+  s.summary     = %q{Guard for rails_best_practices, a code metric tool to check the quality of rails code.}
+  s.description = %q{Guard for rails_best_practices, a code metric tool to check the quality of rails code.}
 
   s.rubyforge_project = "guard-rails_best_practices"
 
---
+++ lib/guard/rails_best_practices.rb
@@ -0,0 +1,65 @@
+require 'guard'
+require 'guard/guard'
+require File.join(File.dirname(__FILE__), "guard-rails_best_practices/version")
+
+module Guard
+  class Rails_best_practices < Guard
+    autoload :Notifier, 'guard/annotate/notifier'
+
+    def initialize(watchers = [], options = {})
+      super
+      options[:vendor]   = true   if options[:vendor].nil?
+      options[:spec]     = true   if options[:spec].nil?
+      options[:test]     = true   if options[:test].nil?
+      options[:features] = true   if options[:features].nil?
+      options[:exclude]  = ''     if options[:exclude].nil?
+
+      options[:run_at_start] = true if options[:run_at_start].nil?
+    end
+
+    def start
+      run_bestpractices if options[:run_at_start]
+    end
+
+    def stop
+      true
+    end
+
+    def reload
+      run_bestpractices
+    end
+
+    def run_all
+      run_bestpractices
+    end
+
+    def run_on_change(paths)
+      run_bestpractices
+    end
+
+    def run_on_deletion(paths)
+    end
+
+    def notify?
+      !!options[:notify]
+    end
+
+  private
+    def run_bestpractices
+      UI.info 'Running Rails Best Practices checklist', :reset => true
+      started_at = Time.now
+
+      cmd = 'rails_best_practices'
+      cmd += ' --vendor'   if options[:vendor]
+      cmd += ' --spec'     if options[:spec]
+      cmd += ' --test'     if options[:test]
+      cmd += ' --features' if options[:features]
+      cmd += " --exclude #{options[:exclude]}" unless options[:exclude].blank?
+      @result = system(cmd)
+
+      Notifier::notify( @result, Time.now - started_at ) if notify?
+      @result
+    end
+
+  end
+end
